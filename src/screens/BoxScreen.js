bf027d80acbbe89a5337c1da22b9d99efd62046b
liquid54 <dk.sudent.course@gmail.com>
2024-07-25 02:15:41 +0300

    new project

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,11 +4,25 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
     return (
         <View>
-            <Text>Box Screen</Text>
+            <Text style={styles.textStyle}>Box Screen</Text>
+            <Text style={styles.textStyle}>Box Screen</Text>
+            <Text style={styles.textStyle}>Box Screen</Text>
+
         </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+    },
+    textStyle: {
+        borderWidth: 10,
+        borderColor: 'red',
+        marginVertical: 20,
+        marginHorizontal: 20
+    }
+});
 
 export default BoxScreen;
